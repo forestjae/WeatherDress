513b06625245e9c6f6cfad816fe58cd50ee7b8b6
SJ <forestsj30@gmail.com>
2022-03-29 00:41:53 +0900

    feat: StoryBoard 제거 및 초기 설정

---
+++ WeatherDress/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/03/29.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setController()
+    }
+
+    private func setController() {
+        self.view.backgroundColor = .white
+    }
+}

f9801e423fb29868a9a95e9b302bdcab0b039774
SJ <forestsj30@gmail.com>
2022-04-05 21:36:06 +0900

    feat: HTTPMethod 열거형 구현

---
+++ WeatherDress/Networking/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/05.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case update = "UPDATE"
+    case delete = "DELETE"
+}

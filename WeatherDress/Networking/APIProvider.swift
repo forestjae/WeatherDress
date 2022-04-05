b7365945a75ff6900447a5ad84a6b13550809859
SJ <forestsj30@gmail.com>
2022-04-05 21:38:46 +0900

    feat: APIProvider 클래스 구현

--- WeatherDress/Networking/APIProvider.swift
+++ WeatherDress/Networking/APIProvider.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class APIProvider {
+    let session = URLSession.shared
+
+    func request<T: APIRequest>(
+        request: T,
+        completion: @escaping (Result<Data, NetworkingError>) -> Void
+    ) {
+        guard let urlRequest = request.urlReqeust else {
+            return
+        }
+
+        let task = self.session.dataTask(with: urlRequest) { data, response, error in
+            if error != nil {
+                completion(.failure(.serverError))
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                      return completion(.failure(.invalidResponse))
+                  }
+
+            guard let data = data else {
+                return completion(.failure(.invalidData))
+            }
+
+            return completion(.success(data))
+        }
+        task.resume()
+    }
+}

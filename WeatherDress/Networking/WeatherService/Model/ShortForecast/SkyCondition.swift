23a02000db96d6f6688166aca54750360ae93780
SJ <forestsj30@gmail.com>
2022-04-05 21:49:03 +0900

    feat: 단기예보 하늘상태 열거형 구현

---
+++ WeatherDress/Networking/WeatherService/Model/ShortForecast/SkyCondition.swift
@@ -0,0 +1,14 @@
+//
+//  SkyCondition.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/05.
+//
+
+import Foundation
+
+enum SkyCondition: String {
+    case clear = "1"
+    case cloudy = "3"
+    case hazy = "4"
+}

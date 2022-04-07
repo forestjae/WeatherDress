0dc0aa855544dd56dabc59c5ce088d5763a2d577
SJ <forestsj30@gmail.com>
2022-04-07 13:52:18 +0900

    feat: Geocode 응답타입 구현

---
+++ WeatherDress/Networking/GeoSearchService/Response/GeoCodeResponse.swift
@@ -0,0 +1,53 @@
+//
+//  GeocodeResponse.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/07.
+//
+
+import Foundation
+
+struct GeocodeResponse: Codable {
+    let meta: Meta
+    let documents: [AddressSet]
+
+    struct AddressSet: Codable {
+        let regionType, addressName, region1DepthName, region2DepthName: String
+        let region3DepthName, region4DepthName, code: String
+        let xCoordinate, yCoordinate: Double
+
+        enum CodingKeys: String, CodingKey {
+            case regionType = "region_type"
+            case addressName = "address_name"
+            case region1DepthName = "region_1depth_name"
+            case region2DepthName = "region_2depth_name"
+            case region3DepthName = "region_3depth_name"
+            case region4DepthName = "region_4depth_name"
+            case code
+            case xCoordinate = "x"
+            case yCoordinate = "y"
+        }
+    }
+
+    struct Meta: Codable {
+        let totalCount: Int
+
+        enum CodingKeys: String, CodingKey {
+            case totalCount = "total_count"
+        }
+    }
+}
+
+extension LocationInfo {
+    init?(geocodeAddress: GeocodeResponse.AddressSet) {
+        self.longtitude = geocodeAddress.xCoordinate
+        self.latitude = geocodeAddress.yCoordinate
+        self.address = Address(
+            fullAddress: geocodeAddress.addressName,
+            firstRegion: geocodeAddress.region1DepthName,
+            secondRegion: geocodeAddress.region2DepthName,
+            thirdRegion: geocodeAddress.region3DepthName,
+            fourthRegion: geocodeAddress.region4DepthName
+        )
+    }
+}

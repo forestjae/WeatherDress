fd284691814128b23515987dee5884dc2880ec68
SJ <forestsj30@gmail.com>
2022-04-07 13:55:03 +0900

    feat: 초단기예보 날씨요소 구현

---
+++ WeatherDress/Networking/WeatherService/Model/UltraShortForecast/UltraShortForecastWeatherComponent.swift
@@ -0,0 +1,18 @@
+//
+//  UltraShortForecastWeatherComponent.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/06.
+//
+
+import Foundation
+
+enum UltraShortForecastWeatherComponent: String {
+    case temperature = "T1H"
+    case rainfallForAnHour = "RN1"
+    case skyCondition = "SKY"
+    case humidity = "REH"
+    case rainfallType = "PTY"
+    case lightning = "LGT"
+    case windSpeed = "WSD"
+}

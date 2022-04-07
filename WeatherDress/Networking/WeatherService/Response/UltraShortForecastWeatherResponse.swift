8191e8d4f6389b55997c918fecd23597cd5c16dc
SJ <forestsj30@gmail.com>
2022-04-07 13:54:41 +0900

    feat: 초단기예보 응답타입 구현

---
+++ WeatherDress/Networking/WeatherService/Response/UltraShortForecastWeatherResponse.swift
@@ -0,0 +1,11 @@
+//
+//  UltraShortForecastWeatherResponse.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/06.
+//
+
+import UIKit
+
+typealias UltraShortForecastWeatherResponse = ShortForecastWeatherResponse
+typealias UltraShortForecastWeatherComponentItem = ShortForecastWeatherComponentItem

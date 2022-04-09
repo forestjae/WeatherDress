54fc2eb95cd05885c207e5da77bc214af1f6b4f5
SJ <forestsj30@gmail.com>
2022-04-10 00:47:32 +0900

    feat: 날씨 UseCase 구현

---
+++ WeatherDress/Domain/UseCase/WeatherUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherUsecase.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/05.
+//
+
+import Foundation
+import RxSwift
+
+final class WeatherUseCase {
+    let repository: WeatherRepository
+
+    init(repository: WeatherRepository) {
+        self.repository = repository
+    }
+
+    func fetch(from location: LocationInfo) -> Observable<UltraShortNowcastWeatherItem?> {
+        return self.repository.fetch(from: location)
+    }
+
+    func fetchHourlWeatehr(from location: LocationInfo) -> Observable<[HourlyWeather]> {
+        return self.repository.fetchHourlyWeathers(from: location)
+    }
+
+    func fetchDailyWeather(from location: LocationInfo) -> Observable<[DailyWeather]> {
+        return self.repository.fetchDailyWeathers(from: location)
+    }
+
+    /*
+     fetchCurrentWeather
+     fetchHourlyWeather
+     fetchDailyWeather
+
+     fetchCurrentLoaction
+     fetchSearchResult
+     */
+}

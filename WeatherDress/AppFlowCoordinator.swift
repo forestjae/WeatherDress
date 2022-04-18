b6e71cdd90f964af88d36a518de8af2f3a515d5f
SJ <forestsj30@gmail.com>
2022-04-18 23:35:09 +0900

    feat: AppFlowCoordinator 구현

---
+++ WeatherDress/AppFlowCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  AppFlowCoordinator.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/12.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+class AppFlowCoordinator: Coordinator<Void> {
+    var window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    override func start() -> Observable<Void> {
+        let mainViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        navigationController.setNavigationBarHidden(true, animated: false)
+
+        let pageSceneCoordinator = PageSceneCoordinator(navigationController: navigationController)
+
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+
+        return self.coordinate(to: pageSceneCoordinator)
+    }
+}

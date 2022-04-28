639e395dce931628a04f9b722d0e5c4e3ada5413
SJ <forestsj30@gmail.com>
2022-04-28 12:42:32 +0900

    feat: LocationSearchResultTableViewCell 구현

---
+++ WeatherDress/Presentation/Scene/Location/View/LocationSearchResultTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  LocationSearchResultTableViewCell.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/11.
+//
+
+import UIKit
+
+class LocationSearchResultTableViewCell: UITableViewCell {
+
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.configureCell()
+        self.configureHierarchy()
+        self.configureConstraint()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with string: String, indexPath: Int) {
+        self.locationLabel.text = string
+    }
+
+    private func configureCell() {
+        self.backgroundColor = .clear
+        self.contentView.backgroundColor = .clear
+    }
+
+    private func configureHierarchy() {
+        self.contentView.addSubview(self.locationLabel)
+    }
+
+    private func configureConstraint() {
+        self.locationLabel.snp.makeConstraints {
+            $0.leading.equalTo(self.contentView).offset(40)
+            $0.centerY.equalTo(self.contentView)
+        }
+    }
+}

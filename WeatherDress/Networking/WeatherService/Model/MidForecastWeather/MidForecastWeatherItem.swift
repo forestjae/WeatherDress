289df6be553d92e2b9e6bbe9194e2c2542b66e8d
SJ <forestsj30@gmail.com>
2022-04-28 12:47:09 +0900

    refactor: 중기예보아이템이 하늘상태를 포함하도록 수정

--- WeatherDress/Networking/WeatherService/Model/MidForecastWeather/MidForecastWeatherItem.swift
+++ WeatherDress/Networking/WeatherService/Model/MidForecastWeather/MidForecastWeatherItem.swift
@@ -11,8 +11,30 @@ struct MidForecastWeatherItem {
     let date: Int
     //let beforeNoonWeatehrDescription: String
     let afterNoonWeatherDescription: String
+    let weatherCondtion: MidRoughForecast
     //let beforeNoonRainfallProbability: Int
     let afterNoonRainfallProbability: Int
+
+    init(date: Int, afterNoonWeatherDescription: String, afterNoonRainfallProbability: Int) {
+        self.date = date
+        self.afterNoonWeatherDescription = afterNoonWeatherDescription
+        self.afterNoonRainfallProbability = afterNoonRainfallProbability
+        self.weatherCondtion = MidRoughForecast(rawValue: self.afterNoonWeatherDescription) ?? .clear
+    }
+
+    enum MidRoughForecast: String {
+        case clear = "맑음"
+        case partlyCloudy = "구름많음"
+        case partlyCloudyAndRainy = "구름많고 비"
+        case partlyCloudyAndSnowy = "구름많고 눈"
+        case partlyCloudyAndSleet = "구름많고 비/눈"
+        case partlyCloudyAndShower = "구름많고 소나기"
+        case cloudy = "흐림"
+        case cloudyAndRainy = "흐리고 비"
+        case cloudyAndSnowy = "흐리고 눈"
+        case cloudyAndSleet = "흐리고 비/눈"
+        case cloudyAndShower = "흐리고 소나기"
+    }
 }
 
 struct MidForecastWeaherItemList {

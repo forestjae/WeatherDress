e09a90927985ba012ab67073d787ec61e1cf0a5d
SJ <forestsj30@gmail.com>
2022-04-28 12:52:40 +0900

    feat: 중기온도, 중기기상, 단기예보 요청 메서드 구현
    - 컨벤션에 맞춘 스타일 수정

--- WeatherDress/Networking/WeatherService/WeatherService.swift
+++ WeatherDress/Networking/WeatherService/WeatherService.swift
@@ -33,7 +33,12 @@ final class WeatherService {
                 switch result {
                 case .success(let data):
                     let decoder = JSONDecoder()
-                    guard let decoded = try? decoder.decode(UltraShortNowcastWeatherResponse.self, from: data) else { return }
+                    guard let decoded = try? decoder.decode(
+                        UltraShortNowcastWeatherResponse.self,
+                        from: data
+                    ) else {
+                        return
+                    }
                     let weaterComponents = decoded.response.body?.items.item ?? []
                     guard let resultWeather = UltraShortNowcastWeatherItem(items: weaterComponents) else {
                         return
@@ -54,12 +59,12 @@ final class WeatherService {
         return Single.create { single in
             let request = ShortForecastRequest(
                 function: .ultraShortForecast,
+                baseDate: Date() - 45 * 60 - 1,
                 xAxisNumber: xAxisNumber,
                 yAxisNumber: yAxisNumber,
                 serviceKey: self.serviceKey
             )
             self.apiProvider.request(request) { result in
-                print("통신 완료")
                 switch result {
                 case .success(let data):
                     let decoder = JSONDecoder()
@@ -67,15 +72,104 @@ final class WeatherService {
                         UltraShortForecastWeatherResponse.self,
                         from: data
                     ) else {
-                        print("통신 에러")
                         return
                     }
                     let weaterComponents = decoded.response.body.items.item
                     let resultWeather = UltraShortForecastWeatherList(items: weaterComponents)
-                    print(resultWeather.forecastList.count)
                     single(.success(resultWeather))
                 case .failure(let error):
-                    print("Error!!!!!!!!!!!!!!: \(error)")
+                    single(.failure(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func fetchShortForecastWeather(
+        xAxisNumber: Int,
+        yAxisNumber: Int
+    ) -> Single<ShortForecastWeatherList> {
+        print("Short:\(Date())")
+        return Single.create { single in
+            let request = ShortForecastRequest(
+                function: .shortForecast,
+                baseDate: Date() - 2 * 3600,
+                xAxisNumber: xAxisNumber,
+                yAxisNumber: yAxisNumber,
+                serviceKey: self.serviceKey
+            )
+            self.apiProvider.request(request) { result in
+                switch result {
+                case .success(let data):
+                    let decoder = JSONDecoder()
+                    guard let decoded = try? decoder.decode(
+                        ShortForecastWeatherResponse.self,
+                        from: data
+                    ) else {
+                        return
+                    }
+                    let weaterComponents = decoded.response.body.items.item
+                    let resultWeather = ShortForecastWeatherList(items: weaterComponents)
+                    single(.success(resultWeather))
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func fetchMidForecastWeather(regionCode: String) -> Single<[MidForecastWeatherItem]> {
+        return Single.create { single in
+            let request = MidForecastRequest(
+                function: .midLandForecast,
+                regionIdentification: regionCode,
+                serviceKey: self.serviceKey
+            )
+
+            self.apiProvider.request(request) { result in
+                switch result {
+                case .success(let data):
+                    let decoder = JSONDecoder()
+                    guard let decoded = try? decoder.decode(MidForecastWeatherResponse.self, from: data) else {
+                        return
+                    }
+                    guard let item = decoded.response.body.items.item.first else {
+                        return
+                    }
+
+                    let result = MidForecastWeaherItemList(response: item)
+                    single(.success(result.items))
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func fetchMidForecastTemperature(regionCode: String) -> Single<[MidForecastTemperatureItem]> {
+        return Single.create { single in
+            let request = MidForecastRequest(
+                function: .midTemperatureForecast,
+                regionIdentification: regionCode,
+                serviceKey: self.serviceKey
+            )
+
+            self.apiProvider.request(request) { result in
+                switch result {
+                case .success(let data):
+                    let decoder = JSONDecoder()
+                    guard let decoded = try? decoder.decode(MidForecastTemperatureResponse.self, from: data) else {
+                        return
+                    }
+                    guard let item = decoded.response.body.items.item.first else {
+                        return
+                    }
+                    let result = MidForecastTemperatureItemList(response: item)
+                    single(.success(result.items))
+
+                case .failure(let error):
                     single(.failure(error))
                 }
             }

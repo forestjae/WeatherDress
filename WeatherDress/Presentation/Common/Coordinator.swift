8c55661fe5e9175229399ac6c6a39b9ae7d3f27d
SJ <forestsj30@gmail.com>
2022-04-28 12:18:00 +0900

    feat: Coordinator 클래스 구현
    - 추상클래스 역할

---
+++ WeatherDress/Presentation/Common/Coordinator.swift
@@ -0,0 +1,36 @@
+//
+//  Coordinator.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/27.
+//
+
+import Foundation
+import RxSwift
+
+class Coordinator<CoordinationResult>: NSObject {
+
+    let disposeBag = DisposeBag()
+    let identifer = UUID()
+    var childCoordinators = [UUID: Any]()
+
+    private func store<T>(coordinator: Coordinator<T>) {
+        self.childCoordinators[coordinator.identifer] = coordinator
+    }
+
+    private func release<T>(coordinator: Coordinator<T>) {
+        self.childCoordinators[coordinator.identifer] = nil
+    }
+
+    func coordinate<T>(to coordinator: Coordinator<T>) -> Observable<T> {
+        self.store(coordinator: coordinator)
+        return coordinator.start()
+            .do(onNext: {[weak self] _ in
+                self?.release(coordinator: coordinator)
+            })
+    }
+
+    func start() -> Observable<CoordinationResult> {
+        fatalError()
+    }
+}

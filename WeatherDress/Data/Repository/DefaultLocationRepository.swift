2d5395379e0510413f8302843937d73ec88b1e46
SJ <forestsj30@gmail.com>
2022-04-28 12:44:54 +0900

    refactor: LocationRepository 서비스 호출 부 수정
    - 현 위치와 즐겨찾기 위치 변경  시 자동으로 업데이트 하도록 수정

--- WeatherDress/Data/Repository/DefaultLocationRepository.swift
+++ WeatherDress/Data/Repository/DefaultLocationRepository.swift
@@ -7,23 +7,61 @@
 
 import Foundation
 import RxSwift
+import CoreLocation
 
 final class DefaultLocationRepository: LocationRepository {
-    let apiService: GeoSearchService
     private let disposeBag = DisposeBag()
-    let locations: [LocationInfo] = []
-    let relay = PublishSubject<LocationInfo>()
+    private let apiService: GeoSearchService
+    private let dataBase: LocalDatabaseService
+    private let favoriteLocations: BehaviorSubject<[LocationInfo]>
+    private let currentLocation: BehaviorSubject<LocationInfo?>
+    //private let currentLocation: Observable<CLLocation>
 
-    init(apiService: GeoSearchService) {
+    init(apiService: GeoSearchService, database: LocalDatabaseService) {
         self.apiService = apiService
-        self.apiService.coordinateToAddress(xCoordinate: 127.1, yCoordinate: 37.3414386)
-            .subscribe(onSuccess: {
-                self.relay.onNext($0)
+        self.dataBase = database
+        self.favoriteLocations = BehaviorSubject(value: self.dataBase.fetch())
+        self.currentLocation = BehaviorSubject(value: nil)
+        _ = LocationManager.shared.currentLocation()
+            .flatMap {
+                apiService.coordinateToAddress(
+                xCoordinate: $0.coordinate.longitude,
+                yCoordinate: $0.coordinate.latitude
+                )}
+            .subscribe(onNext: {
+                self.currentLocation.onNext($0)
             })
-            .disposed(by: self.disposeBag)
     }
 
-    func fetchLocations() -> Observable<LocationInfo> {
-        return self.relay.asObserver()
+    func fetchCurrentLocation() -> Observable<LocationInfo?> {
+        return self.currentLocation
+    }
+
+    func fetchFavoriteLocations() -> Observable<[LocationInfo]> {
+        return self.favoriteLocations.asObservable()
+    }
+
+    func deleteLocation(location: LocationInfo) -> Completable {
+        return self.dataBase.delete(location)
+            .andThen(Completable.create { completable in
+                self.favoriteLocations.onNext(self.dataBase.fetch())
+                completable(.completed)
+                return Disposables.create()
+            })
+    }
+
+    func createLocation(location: LocationInfo) -> Completable {
+        return self.dataBase.create(location)
+            .andThen(Completable.create { completable in
+                self.favoriteLocations.onNext(self.dataBase.fetch())
+                completable(.completed)
+                return Disposables.create()
+            })
+    }
+
+    func searchLocation(for query: String) -> Observable<[LocationInfo]> {
+        return self.apiService.searchAddress(by: query).asObservable()
     }
 }
+
+let sharedRepo = DefaultLocationRepository(apiService: GeoSearchService(apiProvider: DefaultAPIProvider()), database: RealmService()!)

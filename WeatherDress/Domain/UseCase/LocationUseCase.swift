5f5f46309d7d47d6682b8074f1439005a9368e7c
SJ <forestsj30@gmail.com>
2022-04-28 12:49:35 +0900

    feat: LocationUseCase 세부 메서드 구현

--- WeatherDress/Domain/UseCase/LocationUseCase.swift
+++ WeatherDress/Domain/UseCase/LocationUseCase.swift
@@ -15,7 +15,25 @@ final class LocationUseCase {
         self.repository = repository
     }
 
-    func fetch() -> Observable<LocationInfo> {
-        return self.repository.fetchLocations()
+    func fetchLocations() -> Observable<[LocationInfo]> {
+        return Observable.combineLatest(
+            self.repository.fetchCurrentLocation().compactMap { $0 },
+            self.repository.fetchFavoriteLocations()
+        ).map { [$0] + $1 }.filter { $0.count != 0 }
+    }
+
+    func deleteLocation(location: LocationInfo) -> Completable {
+        return self.repository.deleteLocation(location: location)
+    }
+
+    func createLocation(location: LocationInfo) -> Completable {
+        return self.repository.createLocation(location: location)
+    }
+
+    func search(for query: String) -> Observable<[LocationInfo]> {
+        if query == "" {
+            return self.repository.searchLocation(for: " ")
+        }
+        return self.repository.searchLocation(for: query)
     }
 }

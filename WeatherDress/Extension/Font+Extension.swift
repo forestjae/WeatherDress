0639eaa37dca8f5602ae85f1c86964ce69906117
SJ <forestsj30@gmail.com>
2022-05-02 10:47:50 +0900

    feat: UIFontMetrics 유틸 메서드 익스텐션 구현

---
+++ WeatherDress/Extension/Font+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Font+Extension.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/28.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    func metrics(for textStyle: UIFont.TextStyle) -> UIFont {
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: self)
+    }
+}

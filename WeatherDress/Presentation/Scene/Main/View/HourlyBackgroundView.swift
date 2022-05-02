bf41d5bdc2ceebcefd750858e01a24f2890cd9e3
SJ <forestsj30@gmail.com>
2022-05-02 11:27:27 +0900

    feat: 날씨 CollectionView 배경 뷰 구현

---
+++ WeatherDress/Presentation/Scene/Main/View/HourlyBackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  HourlyBackgroundView.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/04/30.
+//
+
+import UIKit
+
+class HourlyBackgroundView: UICollectionReusableView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.deepSky
+        self.layer.cornerRadius = 10
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

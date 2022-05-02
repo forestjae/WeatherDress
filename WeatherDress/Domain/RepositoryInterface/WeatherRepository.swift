4f360bad62a4070c7ed6ee5519ec87c0856a369f
SJ <forestsj30@gmail.com>
2022-05-02 10:50:44 +0900

    refactor: Repostiry 인터페이스 반환타입 및 메서드명 수정

--- WeatherDress/Domain/RepositoryInterface/WeatherRepository.swift
+++ WeatherDress/Domain/RepositoryInterface/WeatherRepository.swift
@@ -9,7 +9,7 @@ import Foundation
 import RxSwift
 
 protocol WeatherRepository {
-    func fetch(from location: LocationInfo) -> Observable<UltraShortNowcastWeatherItem?>
+    func fetchCurrentWeather(from location: LocationInfo) -> Observable<CurrentWeather>
     func fetchHourlyWeathers(from location: LocationInfo) -> Observable<[HourlyWeather]>
     func fetchDailyWeathers(from location: LocationInfo) -> Observable<[DailyWeather]>
 }

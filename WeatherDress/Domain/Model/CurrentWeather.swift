d1a81b2638a8b7ab304cc2932650711c852a3178
SJ <forestsj30@gmail.com>
2022-05-03 15:32:42 +0900

    refactor: CurrentWeather 모델타입 기상상태 프로퍼티 추가

--- WeatherDress/Domain/Model/CurrentWeather.swift
+++ WeatherDress/Domain/Model/CurrentWeather.swift
@@ -6,10 +6,10 @@
 //
 
 import Foundation
-import RxSwift
 
 struct CurrentWeather: Hashable {
     let temperature: Double
+    let weatherCondition: WeatherCondition
     let rainfallForAnHour: Double
     let rainfallType: UltraShortNowcastRainfall
     let humidity: Double

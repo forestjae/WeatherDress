aa77463a6ec61e05ced3846dd04130665aaf31a2
SJ <forestsj30@gmail.com>
2022-05-04 18:37:26 +0900

    feat: Calendar Extension 구현
    - 오늘, 내일, 모레 계산 프로퍼티 구현
    - 현재 몇일인지 계산하는 메서드 구현

---
+++ WeatherDress/Extension/Calendar+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Calendar+Extension.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/05/04.
+//
+
+import Foundation
+
+extension Calendar {
+
+    static func day(from date: Date) -> Int? {
+        return Calendar.current.dateComponents([.day], from: date).day
+    }
+
+    static var today: Int? {
+        return Self.current.dateComponents([.day], from: Date()).day
+    }
+
+    static var tommorrow: Int? {
+        return Self.current.dateComponents([.day], from: Date() + 3600 * 24).day
+    }
+
+    static var theDayAfterTommorrow: Int? {
+        return Self.current.dateComponents([.day], from: Date() + 3600 * 48).day
+    }
+}

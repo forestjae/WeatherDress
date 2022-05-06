bb059d56e695cd93db0b46b28e0c483c498ccdbf
SJ <forestsj30@gmail.com>
2022-05-07 00:30:47 +0900

    refactor: PageScene 코디네이터 start메서드 수정
    - subscribe 하지 않도록 수정
    - 메서드 분리
    - 하위 뷰, 뷰모델 초기화 방식 변경

--- WeatherDress/Presentation/Scene/Main/Coordinator/PageSceneCoordinator.swift
+++ WeatherDress/Presentation/Scene/Main/Coordinator/PageSceneCoordinator.swift
@@ -10,76 +10,85 @@ import UIKit
 import RxSwift
 
 class PageSceneCoordinator: Coordinator<Void> {
-    
+
     private let navigationController: UINavigationController
     private let mainViewController: MainViewController
 
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
-        self.mainViewController = navigationController.viewControllers[0] as? MainViewController ?? MainViewController()
-        let viewModel = MainViewModel(
-            useCase: LocationUseCase(repository: sharedRepo))
-        mainViewController.viewModel = viewModel
+        let mainViewController = MainViewController()
+        self.mainViewController = mainViewController
+
     }
 
     override func start() -> Observable<Void> {
+        let viewModel = MainViewModel(
+            useCase: LocationUseCase(repository: sharedRepo), coordinator: self)
+        self.mainViewController.viewModel = viewModel
+        self.navigationController.setViewControllers([self.mainViewController], animated: true)
 
-        guard let viewModel = self.mainViewController.viewModel else {
-            return Observable.never()
-        }
-
-        viewModel.locations
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: {
-                let viewControllerCount = self.mainViewController.orderedViewControllers.count
-                if  viewControllerCount == 0 {
-                    self.mainViewController.orderedViewControllers = $0.enumerated().map { index, lc in
-                        let vc = self.makeWeatherViewController(with: lc)
-                        vc.view.tag = index
-                        return vc
-                    }
-
-                    self.mainViewController.setCurrentPageViewController(at: 0)
-                } else if viewControllerCount < $0.count {
-                    guard let last = $0.last else {
-                        return
-                    }
-                    let vc = self.makeWeatherViewController(with: last)
-                    vc.view.tag = $0.count - 1
-
-                    self.mainViewController.orderedViewControllers.append(vc)
-                    self.mainViewController.setCurrentPageViewController(at: 0)
-                } else if viewControllerCount > $0.count {
-                    zip(self.mainViewController.orderedViewControllers, $0)
-                    .forEach { vc, location in
-                        vc.viewModel?.setLocationInfo(location)
-                    }
-
-                    self.mainViewController.orderedViewControllers.remove(at: $0.count )
-                    self.mainViewController.setCurrentPageViewController(at: 0)
-
-                }
-            })
-            .disposed(by: self.disposeBag)
-
-        viewModel.locationButtonDidTap
-            .flatMap {
-                self.coordinate(to: LocationListCoordinator(parentViewController: self.navigationController))
-            }
-            .subscribe(onNext: { result in
-                switch result {
-                case .cellDidTap(let index):
-                    self.mainViewController.setCurrentPageViewController(at: index)
-                }
-            })
-            .disposed(by: self.disposeBag)
         return Observable.never()
     }
 
-    func makeWeatherViewController(with location: LocationInfo) -> WeatherViewController {
-        let vm = WeatherViewModel(useCase: WeatherUseCase(repository: DefaultWeatherRepository(apiService: WeatherService(apiProvider: DefaultAPIProvider()))), location: location)
+    func coordinateToLocationList() -> Observable<Int> {
+        return self.coordinate(
+            to: LocationListCoordinator(
+                parentViewController:
+                    self.navigationController
+            )
+        )
+            .map { result in
+                switch result {
+                case .cellDidTap(let index):
+                    return index
+                }
+            }
+    }
+
+    func setChildViewController(with locations: [LocationInfo]) {
+        let childCount = self.mainViewController.orderedViewControllers.count
+        if  childCount == 0 {
+            self.mainViewController.orderedViewControllers = locations.enumerated()
+                .map { index, lc in
+                    let vc = self.makeWeatherViewController(with: lc)
+                    vc.view.tag = index
+                    return vc
+                }
+
+            self.mainViewController.setCurrentPageViewController(at: 0)
+        } else if childCount < locations.count {
+            guard let last = locations.last else {
+                return
+            }
+            let vc = self.makeWeatherViewController(with: last)
+            vc.view.tag = locations.count - 1
+
+            self.mainViewController.orderedViewControllers.append(vc)
+            self.mainViewController.setCurrentPageViewController(at: 0)
+        } else if childCount > locations.count {
+            zip(self.mainViewController.orderedViewControllers, locations)
+            .forEach { vc, location in
+                vc.viewModel?.setLocationInfo(location)
+            }
+
+            self.mainViewController.orderedViewControllers.remove(at: locations.count)
+            self.mainViewController.setCurrentPageViewController(at: 0)
+        }
+    }
+
+    private func makeWeatherViewController(with location: LocationInfo) -> WeatherViewController {
+        let viewModel = WeatherViewModel(
+            useCase: WeatherUseCase(
+                repository: DefaultWeatherRepository(
+                    apiService: WeatherService(
+                        apiProvider: DefaultAPIProvider()
+                    )
+                )
+            ),
+            location: location
+        )
         let weatherViewController = WeatherViewController()
-        weatherViewController.viewModel = vm
+        weatherViewController.viewModel = viewModel
         return weatherViewController
     }
 }

665a1dfbd4bd3e667434c49952dab4c3ec466ed7
SJ <forestsj30@gmail.com>
2022-05-25 22:33:09 +0900

    feat: 모든 추천옷차림 화면 뷰모델 구현

---
+++ WeatherDress/Presentation/Scene/AllClothing/ViewModel/AllClothingViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  AllClothingViewModel.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/05/21.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class AllClothingViewModel {
+    let cancel: Observable<Void>
+
+    private let clothes: Observable<[ClothesItemViewModel]>
+    private let _cancel = PublishSubject<Void>()
+
+    init(
+        coordinator: AllClothingCoordinator,
+        clothes: Observable<[ClothesItemViewModel]>
+    ) {
+        self.clothes = clothes.share(replay: 1)
+        self.cancel = self._cancel.asObservable()
+    }
+
+    struct Input {
+        let cancelButtonDidTapped: Observable<Void>
+    }
+
+    struct Output {
+        let outerClothingItem: Driver<[ClothesItemViewModel]>
+        let topClothingItem: Driver<[ClothesItemViewModel]>
+        let bottomsClothingItem: Driver<[ClothesItemViewModel]>
+        let accessoryClothingItem: Driver<[ClothesItemViewModel]>
+    }
+
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        input.cancelButtonDidTapped
+            .subscribe(self._cancel)
+            .disposed(by: disposeBag)
+
+        let outerClotingItemViewModel = self.clothes
+            .map {
+                $0.filter { $0.type == .outer }
+            }
+            .asDriver(onErrorJustReturn: [])
+
+        let topClotingItemViewModel = self.clothes
+            .map {
+                $0.filter { $0.type == .top }
+            }
+            .asDriver(onErrorJustReturn: [])
+
+        let bottomsClotingItemViewModel = self.clothes
+            .map {
+                $0.filter { $0.type == .bottoms }
+            }
+            .asDriver(onErrorJustReturn: [])
+
+        let accessoryClotingItemViewModel = self.clothes
+            .map {
+                $0.filter { $0.type == .accessory }
+            }
+            .asDriver(onErrorJustReturn: [])
+
+        return Output(
+            outerClothingItem: outerClotingItemViewModel,
+            topClothingItem: topClotingItemViewModel,
+            bottomsClothingItem: bottomsClotingItemViewModel,
+            accessoryClothingItem: accessoryClotingItemViewModel
+        )
+    }
+}

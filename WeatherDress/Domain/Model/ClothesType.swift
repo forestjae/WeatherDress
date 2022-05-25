d36656fdfef246edb9f47d0e1b4a03cf299f5729
SJ <forestsj30@gmail.com>
2022-05-25 22:11:21 +0900

    feat: ClothesType 모델 CaseIterable 채택 및 description 추가

--- WeatherDress/Domain/Model/ClothesType.swift
+++ WeatherDress/Domain/Model/ClothesType.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-enum ClothesType: String, Decodable {
+enum ClothesType: String, Decodable, CaseIterable {
     case outer
     case top
     case bottoms
     case accessory
+
+    var description: String {
+        switch self {
+        case .outer:
+            return "아우터"
+        case .top:
+            return "상의"
+        case .bottoms:
+            return "하의"
+        case .accessory:
+            return "악세서리"
+        }
+    }
 }

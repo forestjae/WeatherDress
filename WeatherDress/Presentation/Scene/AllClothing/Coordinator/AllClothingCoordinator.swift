fb2cfea9f81fe88b5263da3bb66be07ddde0acad
SJ <forestsj30@gmail.com>
2022-05-25 22:35:17 +0900

    feat: 모든 추천 옷차림 Coordinator 구현

---
+++ WeatherDress/Presentation/Scene/AllClothing/Coordinator/AllClothingCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  AllClothingCoordinator.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/05/25.
+//
+
+import UIKit
+import RxSwift
+
+class AllClothingCoordinator: Coordinator<Void> {
+
+    private let parentViewController: UIViewController
+    private let clothingItems: Observable<[ClothesItemViewModel]>
+
+    init(
+        parentViewController: UIViewController,
+        for clothingItems: Observable<[ClothesItemViewModel]>
+    ) {
+        self.parentViewController = parentViewController
+        self.clothingItems = clothingItems
+    }
+
+    override func start() -> Observable<Void> {
+        let allClothingViewController = AllClothingViewController()
+        let allClothingViewModel = AllClothingViewModel(coordinator: self, clothes: self.clothingItems)
+        allClothingViewController.viewModel = allClothingViewModel
+
+        allClothingViewController.modalPresentationStyle = .overFullScreen
+        allClothingViewController.modalTransitionStyle = .crossDissolve
+
+        self.parentViewController.present(
+            allClothingViewController,
+            animated: true,
+            completion: nil
+        )
+
+        let cancel = allClothingViewModel.cancel
+
+        return cancel
+            .take(1)
+            .do(onNext: { _ in
+                allClothingViewController.dismiss(animated: true, completion: nil)
+            })
+    }
+}

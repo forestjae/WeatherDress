e995b764ef96e451e15eb2da98041993b0998843
SJ <forestsj30@gmail.com>
2022-06-13 20:10:03 +0900

    feat: UserSettingCoodinator 구현

---
+++ WeatherDress/Presentation/Scene/UserSetting/Coordinator/UserSettingCoordinator.swift
@@ -0,0 +1,54 @@
+//
+//  UserSettingCoordinator.swift
+//  WeatherDress
+//
+//  Created by Lee Seung-Jae on 2022/05/25.
+//
+
+import UIKit
+import RxSwift
+
+class UserSettingCoordinator: Coordinator<Void> {
+
+    private let parentViewController: UINavigationController
+
+    init(parentViewController: UINavigationController) {
+        self.parentViewController = parentViewController
+    }
+
+    override func start() -> Observable<Void> {
+        let userSettingViewController = UserSettingViewController()
+        let userSettingRepository = DefaultUserSettingRepository()
+        let userSettingUseCase = UserSetttingUseCase(repository: userSettingRepository)
+        let userSettingViewModel = UserSettingViewModel(
+            useCase: userSettingUseCase,
+            coordinator: self
+        )
+
+        userSettingViewController.viewModel = userSettingViewModel
+
+        self.parentViewController.pushViewController(userSettingViewController, animated: true)
+        self.parentViewController.setNavigationBarHidden(false, animated: true)
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithDefaultBackground()
+            appearance.backgroundColor = .moderateSky
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            self.parentViewController.navigationBar.standardAppearance = appearance
+            self.parentViewController.navigationBar.scrollEdgeAppearance = appearance
+        } else {
+            self.parentViewController.navigationBar.barTintColor = .moderateSky
+            self.parentViewController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        }
+
+        userSettingViewController.navigationController?.navigationBar.barTintColor = .blue
+
+        let backBarButtonDidTap = userSettingViewModel.backBarButtonDidTap
+
+        return backBarButtonDidTap
+            .do(onNext: {
+                self.parentViewController.isNavigationBarHidden = true
+                self.parentViewController.popViewController(animated: true)
+            })
+    }
+}

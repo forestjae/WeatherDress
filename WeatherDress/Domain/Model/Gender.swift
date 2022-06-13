3c3c22b203af2819a1f8069b49e8ada3f88a57d4
SJ <forestsj30@gmail.com>
2022-06-13 20:23:06 +0900

    feat: Gender 타입 설정관련 생성자 구현

--- WeatherDress/Domain/Model/Gender.swift
+++ WeatherDress/Domain/Model/Gender.swift
@@ -23,3 +23,27 @@ enum Gender: String, Decodable {
         }
     }
 }
+
+extension Gender {
+    init?(index: Int) {
+        switch index {
+        case 0:
+            self = .male
+        case 1:
+            self = .female
+        default:
+            return nil
+        }
+    }
+
+    var index: Int? {
+        switch self {
+        case .male:
+            return 0
+        case .female:
+            return 1
+        case .unisex:
+            return nil
+        }
+    }
+}

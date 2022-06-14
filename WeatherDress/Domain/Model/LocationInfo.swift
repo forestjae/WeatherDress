125955b6741565340a21e306bde1122d0b9570ad
SJ <forestsj30@gmail.com>
2022-06-15 00:49:07 +0900

    refactor: 행정동 주소가 없을 시 법정동으로 대체하는 로직 구현

--- WeatherDress/Domain/Model/LocationInfo.swift
+++ WeatherDress/Domain/Model/LocationInfo.swift
@@ -39,6 +39,9 @@ struct LocationInfo: Hashable {
 
 extension LocationInfo {
     init?(searchedAddressSet: AddressSearchResponse.AddressSet) {
+        guard searchedAddressSet.address.region2DepthName != "" else {
+            return nil
+        }
         guard let longtitude = Double(searchedAddressSet.xCoordinate),
               let latitude = Double(searchedAddressSet.yCoordinate) else {
                   return nil
@@ -46,11 +49,19 @@ extension LocationInfo {
         self.identifier = UUID()
         self.longtitude = longtitude
         self.latitude = latitude
+        let thirdRegion: String = {
+            if searchedAddressSet.address.region3DepthName == "" {
+                return searchedAddressSet.address.region3DepthHName
+            } else {
+                return searchedAddressSet.address.region3DepthName
+            }
+        }()
+    
         self.address = Address(
             fullAddress: searchedAddressSet.address.addressName,
             firstRegion: searchedAddressSet.address.region1DepthName,
             secondRegion: searchedAddressSet.address.region2DepthName,
-            thirdRegion: searchedAddressSet.address.region3DepthName,
+            thirdRegion: thirdRegion,
             fourthRegion: nil
         )
         self.isCurrent = false

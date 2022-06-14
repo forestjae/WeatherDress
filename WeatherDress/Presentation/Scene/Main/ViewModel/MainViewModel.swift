2fbc84ea5d807596c4e16c3ec9fa66425c026dfc
SJ <forestsj30@gmail.com>
2022-06-14 14:05:32 +0900

    refactor: 실행 중 위치정보 권한 변경 대응을 위한 MainVM 수정
    - 현재 위치 페이지가 표시되는지를 구분하는 PublishSubject 추가

--- WeatherDress/Presentation/Scene/Main/ViewModel/MainViewModel.swift
+++ WeatherDress/Presentation/Scene/Main/ViewModel/MainViewModel.swift
@@ -24,21 +24,91 @@ final class MainViewModel {
     struct Input {
         let viewWillAppear: Observable<Void>
         let locationButtonDidTap: Observable<Void>
+        let configurationButtonDidTap: Observable<Void>
     }
 
     struct Output {
-        let locations: Driver<[LocationInfo]>
+        let currentLocation: Observable<LocationInfo>
+        let locations: Driver<Int>
         let currentIndex: Driver<Int>
+        let currentLocationAvailable: Driver<Bool>
+        let anyLocationAvailable : Driver<Bool>
     }
 
     func transform(input: Input, disposeBag: DisposeBag) -> Output {
-        let locations = self.useCase.fetchLocations()
+        let currentLocationIsVisible = BehaviorSubject<Bool>(value: false)
+        let currentLocationAvailable = LocationManager.shared.authorizationStatus()
+            .map { status -> Bool in
+                switch status {
+                case .denied:
+                    return false
+                default:
+                    return true
+                }
+            }
+
+        currentLocationIsVisible
+            .subscribe()
+            .disposed(by: self.disposeBag)
+
+        let currentLocation = self.useCase.fetchCurrentLocations()
             .share()
-            .observe(on: MainScheduler.instance)
-            .do(onNext: { [weak self] locations in
-                self?.coordinator.setChildViewController(with: locations)
+
+        let favoriteLocations = input.viewWillAppear
+            .take(1)
+            .flatMap { self.useCase.fetchFavoriteLocations() }
+            .share()
+
+        _ = currentLocation
+            .withLatestFrom(
+                currentLocationIsVisible,
+                resultSelector: { location, isVisible in
+                (location, isVisible)
             })
-            .asDriver(onErrorJustReturn: [])
+            .observe(on: MainScheduler.instance)
+            .do(onNext: { [weak self] location, isVisible1 in
+                self?.coordinator.setCurrentLocationWeatherViewController(
+                    with: location,
+                    isVisible: isVisible1
+                )
+                currentLocationIsVisible.onNext(true)
+            })
+            .subscribe()
+
+        _ = currentLocationAvailable
+                .withLatestFrom(
+                    currentLocationIsVisible,
+                    resultSelector: { isAvailable, isVisible -> Bool in
+                        isVisible == true && isAvailable == false
+                })
+                .filter { $0 == true }
+                .observe(on: MainScheduler.instance)
+                .do(onNext: { [weak self] _ in
+                    self?.coordinator.deleteCurrentLocationWeatherViewController()
+                    currentLocationIsVisible.onNext(false)
+                })
+                .subscribe()
+
+        _ = Observable.combineLatest(
+            favoriteLocations,
+            currentLocationIsVisible.skip(1)
+        )
+            .observe(on: MainScheduler.instance)
+            .do(onNext: { [weak self] locations, isVisible in
+                self?.coordinator.setChildViewController(with: locations, isCurrentVisible: isVisible)
+            })
+            .map { $0.0 }
+            .subscribe()
+
+        let locationCount = Observable.combineLatest(
+            currentLocationAvailable.map { $0 ? 1 : 0},
+            favoriteLocations.map { $0.count }
+        )
+            .map { $0.0 + $0.1 }
+            .asDriver(onErrorJustReturn: 0)
+
+        let anyLocationAvailable = locationCount
+            .map { $0 != 0 }
 
         let currentIndex = input.locationButtonDidTap
             .withUnretained(self.coordinator)
@@ -47,9 +117,26 @@ final class MainViewModel {
             }
             .asDriver(onErrorJustReturn: 0)
 
+        input.configurationButtonDidTap
+            .withUnretained(self.coordinator)
+            .flatMap { coordinator, _ in
+                coordinator.coordinateToUserSetting()
+            }
+            .subscribe()
+            .disposed(by: self.disposeBag)
+
+        currentLocationAvailable
+            .subscribe(onNext: {
+                $0 ? () : currentLocationIsVisible.onNext(false)
+            })
+            .disposed(by: self.disposeBag)
+
         return Output(
-            locations: locations,
-            currentIndex: currentIndex
+            currentLocation: currentLocation,
+            locations: locationCount,
+            currentIndex: currentIndex,
+            currentLocationAvailable: currentLocationAvailable.asDriver(onErrorJustReturn: false),
+            anyLocationAvailable: anyLocationAvailable
         )
     }
 }

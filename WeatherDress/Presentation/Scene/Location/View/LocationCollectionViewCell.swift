3748b1db99f0ace7e47a787c4a1220209f9321a6
SJ <forestsj30@gmail.com>
2022-06-20 15:38:27 +0900

    refactor: 불필요 프레임워크 삭제

--- WeatherDress/Presentation/Scene/Location/View/LocationCollectionViewCell.swift
+++ WeatherDress/Presentation/Scene/Location/View/LocationCollectionViewCell.swift
@@ -5,7 +5,6 @@
 //  Created by Lee Seung-Jae on 2022/05/01.
 //
 
-import Foundation
 import UIKit
 
 private enum Design {
@@ -65,7 +64,7 @@ extension LocationCollectionViewCell {
         }
         content.textProperties.font = .systemFont(ofSize: 20, weight: .bold).metrics(for: .body)
         content.textProperties.color = Design.mainFontColor
-        content.secondaryText = weather.weatherCondition.rawValue
+        content.secondaryText = weather.weatherCondition.description
         content.secondaryTextProperties.font = .preferredFont(forTextStyle: .body)
         content.secondaryTextProperties.color = Design.mainFontColor
         content.textToSecondaryTextVerticalPadding = 35
